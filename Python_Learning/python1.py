cd0972f96ff9989a5420a6ef87ec4ca3059cdff3
Istelly <111928746+Istelly@users.noreply.github.com>
2022-09-10 22:51:33 +0300

    python basics

--- Python_Learning/python1.py
+++ Python_Learning/python1.py
@@ -1,2 +1,49 @@
 #!/bin/python3
 
+#this is a comment
+my_name = "codecademy"
+print("Hello and welcome" + my_name + "!")
+
+#Floating point, Integers, and doubles
+release_year = 1999
+runtime = 120
+rating_out_or_10 = 7.6
+
+#calculations
+print(25 * 68 + 13 / 28)
+
+#Calculations with vairables 
+quilt_width = 8
+quilt_length = 8
+
+print(quilt_width * quilt_length)
+
+#modulo
+my_team = 28 % 4
+print(my_team)
+
+#concatonation
+String1 = "The wind, "
+String2 = "Which had hitherot carried us along with amazing rapidity, "
+string3 = "sank at sunset to a light breeze: " 
+
+message = string1 + string2 + string3
+
+print(message)
+
+#plus equals
+age = 40
+age += 1
+
+print(age)
+
+#multiline screen
+to_you = """
+Stranger, if you passing meet
+me and desire to speak to me,
+why
+    should you not speak to me?
+and why should i not speak to 
+you?
+"""
+print(to_you)
