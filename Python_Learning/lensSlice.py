34d2c9f6564f36a7a3cb0de218a61e2ac4e78e6d
Istelly <111928746+Istelly@users.noreply.github.com>
2022-10-01 22:09:31 +0300

    new python scripts

--- Python_Learning/lensSlice.py
+++ Python_Learning/lensSlice.py
@@ -1,1 +1,11 @@
 
+toppings = ["pepperoni", "pineapple", "cheese", "sausage", "olives", "anchovies", "mushrooms"]
+prices = [2, 6, 1, 3, 2, 7, 2]
+pizza_and_prices[]
+for i in toppings
+    pizza_and_prices.append([])
+
+num_pizzas = len(toppings)
+
+
+print("We sell ", num_pizzas, " different kinds of pizza!")
