8b6596b34116a8b2f5154b9a06417a4bfb70239c
Istelly <111928746+Istelly@users.noreply.github.com>
2022-11-22 06:22:21 +0300

    update

--- Python_Learning/SendKeyloggerComments.py
+++ Python_Learning/SendKeyloggerComments.py
@@ -12,7 +12,7 @@ time.sleep(1)
 
 def send_request():
     cookies = {'PHPSESSID':'u011e9l0t5le0na9rh6ddtc0j2', 'security':'low'}
-    url='127.0.0.1/DVWA/vulnerabilities/xss_s/' 
+    url='http://127.0.0.1/DVWA/vulnerabilities/xss_s/' 
     form_input = open("/home/isaiah/ITP270_001N_FALL2022/Python_Learning/keyboard_Input.txt") 
     form_send = form_input.read() 
     form_data = {'txtName':'Test IT', 'mtxMessage':f"'{form_send}'", 'btnSign':'Sign+Guestbook'}   
